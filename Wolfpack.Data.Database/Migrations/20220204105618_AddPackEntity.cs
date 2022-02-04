0f71d2ceea2b39fdec798ceebb92bf57abf21341
Tim van den Essen <tim.van.den.essen@wolfpackit.nl>
2022-02-04 12:01:45 +0100

    Create initial migration

---
+++ Wolfpack.Data.Database/Migrations/20220204105618_AddPackEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wolfpack.Data.Database.Migrations
+{
+    public partial class AddPackEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Packs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Latitude = table.Column<float>(type: "real(10)", precision: 10, scale: 7, nullable: false),
+                    Longitude = table.Column<float>(type: "real(10)", precision: 10, scale: 7, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Packs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Packs");
+        }
+    }
+}
